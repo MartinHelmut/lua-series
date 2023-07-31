605b757cfa59862cc591bf1b9d6f191801166cb2
Martin Helmut Fieber <info@martin-fieber.se>
2023-07-31 13:57:54 +0200

    Fix casing for asset function getPath

--- src/examples/mock-module/assets.lua
+++ src/examples/mock-module/assets.lua
@@ -3,8 +3,8 @@ local path = require("path")
 local Assets = {}
 
 function Assets:getPath()
-	local user_home = path.user_home()
-	return user_home .. path.DIR_SEP .. "assets"
+	local userHome = path.user_home()
+	return userHome .. path.DIR_SEP .. "assets"
 end
 
 return Assets
