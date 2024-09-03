29f6869477ca284890203107fd3ae8811d84a3aa
Martin Helmut Fieber <info@martin-fieber.se>
2024-09-03 14:14:15 +0200

    Fix typo on variable name

--- src/examples/assert-load.lua
+++ src/examples/assert-load.lua
@@ -1,8 +1,8 @@
 local function readConfig(path)
 	local config = {}
 	assert(loadfile(path, "t", config))()
-    return config
+	return config
 end
 
 local config = readConfig("src/examples/config.lua")
-print(config.speed, config.jumpHight)
+print(config.speed, config.jumpHeight)
